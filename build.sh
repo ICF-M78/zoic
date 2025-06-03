4d550981f744e2ef0053c40d8843358fa7286c6a
wujingtao <wujingtaoo@outlook.com>
2025-06-03 14:37:10 +0800

    保存

--- build.sh
+++ build.sh
@@ -14,19 +14,3 @@ mv ${PROJECT_NAME}.zip /Users/wujingtao/Downloads
 
 # 清理构建目录
 rm -rf ./${PROJECT_NAME} 
-
-# 发布 114.215.187.227 
-# 端口 1234 
-# 账户 norma 
-# 密码 000626
-# 地址 /var/www/pages/zoic
-
-scp -P 1234 /Users/wujingtao/Downloads/zoic.zip norma@114.215.187.227:/var/www/pages
-
-ssh -p 1234 norma@114.215.187.227
-
-# 解压
-unzip zoic.zip
-
-# 删除压缩文件
-rm -rf zoic.zip
