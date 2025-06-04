d4d87e7256726740b34f62b58943baf631017007
wujingtao <wujingtaoo@outlook.com>
2025-06-05 01:00:41 +0800

    1、添加字舞

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,8 +7,30 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: () => import("@/views/home/HomePage.vue"),
+      meta: {
+        title: "壁字",
+      },
+    },
+    {
+      path: "/fd",
+      redirect: "/font-dance",
+    },
+    {
+      path: "/font-dance",
+      name: "fontDance",
+      component: () => import("@/views/font-dance/IndexView.vue"),
+      meta: {
+        title: "字舞",
+      },
     },
   ],
+});
+
+// 前置路由守卫
+router.beforeEach((to, from, next) => {
+  // 设置页面标题
+  document.title = to.meta.title ? `${to.meta.title} - Zoic` : "Zoic";
+  next();
 });
 
 export default router;
---
+++ src/views/font-dance/IndexView.vue
@@ -0,0 +1,147 @@
+<!-- 背景动画 -->
+<template>
+  <div ref="main_div_ref" id="main-div"></div>
+</template>
+<script lang="ts" setup>
+import { onMounted, onUnmounted, ref } from "vue";
+import { fonts } from "./fonts";
+
+/** @description 动画结束 */
+const animationEnd = (event: Event) => {
+  (event.target as Element).removeEventListener("animationend", animationEnd);
+  (event.target as Element).remove();
+};
+
+/** @description 随机文字 */
+const rndContent = () => {
+  return fonts[Math.floor(Math.random() * fonts.length)];
+};
+
+/** @description 随机文字大小 */
+const rndSize = () => {
+  const min_size = 30;
+  const max_size = 100;
+  return Math.floor(Math.random() * (max_size - min_size) + min_size);
+};
+
+/** @description 随机文字大小 */
+const rndSpeed = () => {
+  const min_speed = 1;
+  const max_speed = 4;
+  return Math.random() * (max_speed - min_speed) + min_speed;
+};
+
+/** @description 随机位置 */
+function rndPosition(): { x: number; y: number } {
+  let x: number, y: number;
+  x = Math.floor(Math.random() * window.innerWidth);
+  y = Math.floor(Math.random() * window.innerHeight);
+  return { x, y };
+}
+
+const main_div_ref = ref();
+/** @description 随机动画 */
+const rndAnimation = () => {
+  if (!main_div_ref.value) {
+    return;
+  }
+  // 创建一个新的div
+  const div = document.createElement("div");
+  div.className = "animation-text";
+  div.innerText = rndContent();
+  const { x, y } = rndPosition();
+  div.style.left = x + "px";
+  div.style.top = y + "px";
+  div.style.fontSize = rndSize() + "px";
+  div.style.setProperty("--animation-duration", rndSpeed() + "s");
+  // 绑定事件 animationEnd
+  div.addEventListener("animationend", animationEnd);
+  main_div_ref.value.appendChild(div);
+};
+
+let _t: NodeJS.Timer | null = null;
+/** @description 计时器 */
+const timer = () => {
+  _t && clearInterval(_t);
+  _t = setInterval(() => {
+    rndAnimation();
+  }, 50);
+};
+
+onMounted(() => {
+  timer();
+});
+
+onUnmounted(() => {
+  _t && clearInterval(_t);
+});
+// #endregion
+</script>
+
+<style lang="scss" scoped>
+#main-div {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  // 超出隐藏
+  overflow: hidden;
+}
+
+::v-deep(.animation-text) {
+  position: absolute;
+  top: 50vh;
+  left: 50vw;
+  transform: translate(-50%, -50%);
+  //
+  font-family: "ShuiYunZhuan";
+  background: linear-gradient(
+    30deg,
+    rgb(0, 0, 0),
+    rgb(8, 102, 53),
+    rgb(11, 195, 100)
+  );
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  //
+  animation: text-animation var(--animation-duration) forwards;
+  animation-timing-function: linear;
+}
+
+@keyframes text-animation {
+  0% {
+    opacity: 0.2;
+    transform: scale(0.2);
+  }
+
+  20% {
+    opacity: 0.4;
+    transform: scale(0.3);
+  }
+
+  40% {
+    opacity: 0.6;
+    transform: scale(0.6);
+  }
+
+  60% {
+    opacity: 0.8;
+    transform: scale(0.8);
+  }
+
+  80% {
+    opacity: 1;
+    transform: scale(1);
+  }
+
+  90% {
+    opacity: 0.6;
+    transform: scale(1.6);
+  }
+
+  100% {
+    opacity: 0;
+    transform: scale(2);
+  }
+}
+</style>
